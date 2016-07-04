e4477ae5932d58502b50189c0151bb46ba3cbf20
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-07-04 17:24:14 +0200

    first release of maven plugin

---
+++ src/main/java/io/koara/maven/ConvertMojo.java
@@ -0,0 +1,119 @@
+package io.koara.maven;
+
+import io.koara.Parser;
+import io.koara.ast.Document;
+import io.koara.html.Html5Renderer;
+import io.koara.xml.XmlRenderer;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @goal convert
+ * @requiresProject false
+ */
+
+public class ConvertMojo extends AbstractMojo {
+
+    /**
+     * @parameter expression="${koara.sourceDirectory}" default-value="${basedir}/src/main/koara"
+     */
+    private File sourceDirectory;
+
+    /**
+     * @parameter expression="${koara.outputDirectory}" default-value="${project.build.directory}/generated-docs"
+     */
+    protected File outputDirectory;
+
+    /**
+     * @parameter expression="${koara.outputFormat}" default-value="html5"
+     */
+    private String outputFormat;
+
+    /**
+     * @parameter expression="${koara.modules}" default-value="paragraphs,headings,lists,links,images,formatting,blockquote,code"
+     */
+    private String modules;
+
+    private String[] formattedModules = {"paragraphs", "headings", "lists", "links", "images", "formatting", "blockquote", "code"};
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        validateAttributes();
+
+        try {
+            for (File file : sourceDirectory.listFiles()) {
+                if (file.getName().endsWith(".kd")) {
+                    parse(sourceDirectory, file.getName());
+                }
+            }
+        } catch(IOException e) {
+            throw new MojoFailureException("IOException: " + e.getMessage());
+        }
+    }
+
+    private void parse(File srcdir, String file) throws IOException, MojoFailureException {
+        Parser parser = new Parser();
+        parser.setModules(formattedModules);
+        Document document = parser.parseFile(new File(srcdir, file));
+        String name = file.substring(0, file.length() - 3);
+        if(outputFormat == null || outputFormat.equalsIgnoreCase("html5")) {
+
+            Html5Renderer renderer = new Html5Renderer();
+            document.accept(renderer);
+            writeToFile(new File(outputDirectory, name + ".htm"), renderer.getOutput());
+        } else if(outputFormat.equalsIgnoreCase("xml")) {
+            XmlRenderer renderer = new XmlRenderer();
+            renderer.setDeclarationTag("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            document.accept(renderer);
+            writeToFile(new File(outputDirectory, name + ".xml"), renderer.getOutput());
+        } else {
+            throw new MojoFailureException("Outputformat '" + outputFormat + "' is unknown. Possible values: html5, xml");
+        }
+    }
+
+    private void writeToFile(File file, String content) throws MojoFailureException {
+        try {
+            file.getParentFile().mkdirs();
+            FileWriter fw = new FileWriter(file);
+            fw.write(content);
+            fw.close();
+        } catch(IOException e) {
+            throw new MojoFailureException("Could not write to file " + file.getAbsolutePath() + ".");
+        }
+    }
+
+    private void validateAttributes() throws MojoFailureException {
+        if (outputDirectory == null) {
+            throw new MojoFailureException("outputDirectory must be set.");
+        }
+
+        if(modules != null) {
+            String[] temp = modules.split(",");
+            for(int i=0; i < temp.length; i++) {
+                temp[i] = temp[i].trim().toLowerCase();
+            }
+            formattedModules = temp;
+        }
+    }
+
+
+    public void setSourceDirectory(File sourceDirectory) {
+        this.sourceDirectory = sourceDirectory;
+    }
+
+    public void setOutputDirectory(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    public void setOutputFormat(String outputFormat) {
+        this.outputFormat = outputFormat;
+    }
+
+    public void setModules(String modules) {
+        this.modules = modules;
+    }
+}
---
+++ src/test/java/io/koara/maven/ConvertMojoTest.java
@@ -0,0 +1,92 @@
+package io.koara.maven;
+
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ConvertMojoTest {
+
+    private ConvertMojo mojo;
+    private File srcDir;
+    private File targetDir;
+    @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
+    @Rule public ExpectedException ex = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        srcDir = new File("src/test/resources");
+        targetDir = temporaryFolder.newFolder("target");
+        mojo = new ConvertMojo();
+        mojo.setSourceDirectory(new File("src/test/resources"));
+        mojo.setOutputDirectory(targetDir);
+    }
+
+    @Test
+    public void execute() throws Exception {
+        mojo.execute();
+
+        File[] files = targetDir.listFiles();
+        assertEquals(1, files.length);
+        assertEquals("input.htm", files[0].getName());
+        assertEquals("<p>Hello World!</p>", readFileAsString(files[0]));
+    }
+
+    @Test
+    public void executeWithOutputFormatXml() throws Exception {
+        mojo.setOutputFormat("xml");
+        mojo.execute();
+
+        File[] files = targetDir.listFiles();
+        assertEquals(1, files.length);
+        assertEquals("input.xml", files[0].getName());
+        assertTrue(readFileAsString(files[0]).startsWith("<?xml version"));
+    }
+
+    @Test
+    public void executeWithOutputFormatUnknown() throws Exception {
+        ex.expect(MojoFailureException.class);
+        ex.expectMessage("Outputformat 'bla' is unknown. Possible values: html5, xml");
+        mojo.setOutputFormat("bla");
+        mojo.execute();
+    }
+
+    @Test
+    public void executeWithModulesSet() throws Exception {
+        mojo.setModules(" HEADINGS");
+        mojo.execute();
+        assertEquals("Hello World!", readFileAsString(targetDir.listFiles()[0]));
+    }
+
+    @Test
+    public void executeToDirNotSet() throws Exception {
+        ex.expect(MojoFailureException.class);
+        ex.expectMessage("outputDirectory must be set.");
+        mojo.setOutputDirectory(null);
+        mojo.execute();
+    }
+
+    private String readFileAsString(File file) throws IOException {
+        StringBuffer fileData = new StringBuffer();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+        }
+        reader.close();
+        return fileData.toString();
+    }
+
+}
